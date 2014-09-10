32dd31f95cf6ec8851380db762829ce5bf2e1136
jfakult15 <jfakult@JacobFakult-MBP.local>
2014-09-10 15:12:18 -0400

    First commit! Starting up with file reading and basic command line error checking

--- SIP/main.cpp
+++ SIP/main.cpp
@@ -7,12 +7,57 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+vector<string> lines;
+vector<string> readFile(ifstream &file);
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    if (argc==1)
+    {
+        cout << "Usage: stuff" << "\n";
+        return 1;
+    }
+    
+    for (int i=1; i<argc; i++)
+    {
+        ifstream file(argv[1]);
+        if (file.is_open())
+        {
+            lines=readFile(file);
+            return 0; //we will exit as soon as we find a
+        }
+        /*else
+        {
+            cout << "Couldn't find it!\n";
+        }*/
+        file.close();
+    }
+    
+    //if we found a good file, the program will have terminated by this point
+    //hence we couldn't find the file in any of the parameters
+    cout << "Couldn't find it!\n";
+    
     return 0;
 }
 
+vector<string> readFile(ifstream &file)
+{
+    vector<string> tempLines;
+    while (!file.eof())
+    {
+        string temp;
+        getline(file, temp);
+        
+        if (temp.length()>0)
+        {
+            cout << temp << "\n";
+            tempLines.push_back(temp);
+        }
+    }
+    return tempLines;
+}
