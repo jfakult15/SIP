caf5a1dd308146593ea6f5a813970a4e3a65b795
jfakult15 <jfakult@JacobFakult-MBP.local>
2014-09-10 14:35:20 -0400

    Initial Commit

---
+++ SIP/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SIP
+//
+//  Created by Jacob Fakult on 9/10/14.
+//  Copyright (c) 2014 jfakult15. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
